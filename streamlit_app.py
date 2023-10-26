248b81fb0cd58da11b3113771c455c945d0df64f
Caroline Frasca (Lu) <caroline.frasca@snowflake.com>
2023-10-26 16:02:03 -0400

    switch to RouterQueryEngine

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,8 +105,11 @@ if selected:
             add_to_message_history("user",selected)
             add_to_message_history("assistant",response)
             st.write(response.metadata["selector_result"])
-            st.write(type(response.metadata))
             st.write(type(response.metadata["selector_result"]))
+            st.write(response.metadata.fromkeys("selector_result"))
+            st.write(type(response.metadata["selector_result"]))
+            st.write(response.metadata["selector_result"].dict())
+
 
             # result = str(response.metadata.get("selector_result"))
             # index_of_query_engine_used = result.split("index=",1)[1][0]
